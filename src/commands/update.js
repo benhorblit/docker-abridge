8909ad5f27127474649842cd95fc01a1af180f22
Ben Horblit <benjamin@horblit.com>
2020-01-12 22:47:23 -0700

    Resolve artifact location correctly when updating
    and wait for build to finish before copying

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,5 +1,6 @@
 const { Command } = require("@oclif/command");
 const fs = require("fs");
+const path = require("path");
 const execa = require("execa");
 const Build = require("./build");
 const { getService, basePath } = require("../utils/compose-config");
@@ -10,16 +11,16 @@ class Update extends Command {
   async run() {
     const requested = this.parse().argv;
 
-    requested.sort().forEach(serviceName => {
+    requested.sort().forEach(async serviceName => {
       const abridgeConfig = getService(serviceName)["docker-abridge"];
 
       const [buildCommand, ...buildArgs] = abridgeConfig.build;
-      execa(buildCommand, buildArgs, {
+      await execa(buildCommand, buildArgs, {
         stdio: "inherit",
         cwd: abridgeConfig.context,
       });
 
-      const artifactsDir = abridgeConfig.context + abridgeConfig.artifacts;
+      const artifactsDir = path.resolve(basePath, abridgeConfig.context, abridgeConfig.artifacts);
       fs.readdirSync(artifactsDir).forEach(file =>
         fs.copyFileSync(artifactsDir + file, `${basePath}/docker/artifacts/${file}`)
       );
