cde4fcda556859eecb3b6f53eefb24b824774296
Ben Horblit <benjamin@horblit.com>
2020-01-13 22:20:21 -0700

    Prototype update with listr

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,7 +1,8 @@
 const { Command } = require("@oclif/command");
 const fs = require("fs");
 const path = require("path");
-const execa = require("execa");
+const execa = require("execa-output");
+const Listr = require("listr");
 const Build = require("./build");
 const { getService, basePath } = require("../utils/compose-config");
 
@@ -11,27 +12,43 @@ class Update extends Command {
   async run() {
     const requested = this.parse().argv;
 
-    await Promise.all(
-      requested.sort().map(async serviceName => {
-        const abridgeConfig = getService(serviceName)["docker-abridge"];
-
-        const [buildCommand, ...buildArgs] = abridgeConfig.build;
-        await execa(buildCommand, buildArgs, {
-          stdio: "inherit",
-          cwd: path.resolve(basePath, abridgeConfig.context),
-        });
-
-        const artifactsDir = path.resolve(basePath, abridgeConfig.context, abridgeConfig.artifacts);
-        fs.readdirSync(artifactsDir).forEach(file =>
-          fs.copyFileSync(
-            path.resolve(artifactsDir, file),
-            path.resolve(basePath, "./docker/artifacts", file)
-          )
-        );
-      })
+    const tasks = new Listr(
+      requested.map(serviceName => ({
+        title: `Build ${serviceName}`,
+        task: () => {
+          const abridgeConfig = getService(serviceName)["docker-abridge"];
+          const [buildCommand, ...buildArgs] = abridgeConfig.build;
+          return execa(buildCommand, buildArgs, {
+            cwd: path.resolve(basePath, abridgeConfig.context),
+          });
+        },
+      })),
+      { concurrent: 2 }
     );
 
-    Build.run(requested);
+    tasks.run().catch(console.error);
+
+    // await Promise.all(
+    //   requested.sort().map(async serviceName => {
+    //     const abridgeConfig = getService(serviceName)["docker-abridge"];
+
+    //     const [buildCommand, ...buildArgs] = abridgeConfig.build;
+    //     await execa(buildCommand, buildArgs, {
+    //       stdio: "inherit",
+    //       cwd: path.resolve(basePath, abridgeConfig.context),
+    //     });
+
+    //     const artifactsDir = path.resolve(basePath, abridgeConfig.context, abridgeConfig.artifacts);
+    //     fs.readdirSync(artifactsDir).forEach(file =>
+    //       fs.copyFileSync(
+    //         path.resolve(artifactsDir, file),
+    //         path.resolve(basePath, "./docker/artifacts", file)
+    //       )
+    //     );
+    //   })
+    // );
+
+    // Build.run(requested);
   }
 }
 
