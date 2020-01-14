95ee1ed7500dbb52041db04b43128536fdeb54ad
Ben Horblit <benjamin@horblit.com>
2020-01-13 22:28:03 -0700

    Add copying

--- src/commands/update.js
+++ src/commands/update.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const execa = require("execa-output");
 const Listr = require("listr");
-const Build = require("./build");
+const { Observable } = require("rxjs");
+// const Build = require("./build");
 const { getService, basePath } = require("../utils/compose-config");
 
 class Update extends Command {
@@ -14,13 +15,40 @@ class Update extends Command {
 
     const tasks = new Listr(
       requested.map(serviceName => ({
-        title: `Build ${serviceName}`,
+        title: serviceName,
         task: () => {
           const abridgeConfig = getService(serviceName)["docker-abridge"];
-          const [buildCommand, ...buildArgs] = abridgeConfig.build;
-          return execa(buildCommand, buildArgs, {
-            cwd: path.resolve(basePath, abridgeConfig.context),
-          });
+          return new Listr([
+            {
+              title: `Build ${serviceName}`,
+              task: () => {
+                const [buildCommand, ...buildArgs] = abridgeConfig.build;
+                return execa(buildCommand, buildArgs, {
+                  cwd: path.resolve(basePath, abridgeConfig.context),
+                });
+              },
+            },
+            {
+              title: `Copy ${serviceName} artifacts`,
+              task: () => {
+                return new Observable(observer => {
+                  observer.next("Copying...");
+                  const artifactsDir = path.resolve(
+                    basePath,
+                    abridgeConfig.context,
+                    abridgeConfig.artifacts
+                  );
+                  fs.readdirSync(artifactsDir).forEach(file =>
+                    fs.copyFileSync(
+                      path.resolve(artifactsDir, file),
+                      path.resolve(basePath, "./docker/artifacts", file)
+                    )
+                  );
+                  observer.complete();
+                });
+              },
+            },
+          ]);
         },
       })),
       { concurrent: 2 }
