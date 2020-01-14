9e08ed02b4932cee409b40749410a7be7dedf9d2
Ben Horblit <benjamin@horblit.com>
2020-01-13 20:29:02 -0700

    Activate services before building them
    since docker-compose needs them to be in the yaml file

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,10 +1,11 @@
 const DockerComposeCommand = require("../utils/docker-compose-command");
-const { updateDeployment } = require("../utils/compose-utils");
+const { updateDeployment, activateServices } = require("../utils/compose-utils");
 
 class Build extends DockerComposeCommand {
   static strict = false;
 
   async run() {
+    activateServices(this.parse().argv);
     await this.dockerCompose("build");
     updateDeployment();
   }
