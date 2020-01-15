6a9b1d5e160fd4f12e83368d01c577c9a617a91b
Ben Horblit <benjamin@horblit.com>
2020-01-14 23:34:21 -0700

    Incorporate image build into listr

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,10 +1,11 @@
 const { Command } = require("@oclif/command");
 const fs = require("fs");
 const { resolve } = require("path");
-const execa = require("execa-output");
+const execao = require("execa-output");
 const Listr = require("listr");
 const { Observable } = require("rxjs");
 const { getService, basePath } = require("../utils/compose-config");
+const { activateServices, updateDeployment } = require("../utils/compose-utils");
 
 class Update extends Command {
   static strict = false;
@@ -22,7 +23,7 @@ class Update extends Command {
               title: "Execute build command",
               task: () => {
                 const [buildCommand, ...buildArgs] = abridgeConfig.build;
-                return execa(buildCommand, buildArgs, {
+                return execao(buildCommand, buildArgs, {
                   cwd: resolve(basePath, abridgeConfig.context),
                 });
               },
@@ -37,15 +38,31 @@ class Update extends Command {
                     abridgeConfig.context,
                     abridgeConfig.artifacts
                   );
-                  fs.readdirSync(artifactsDir).forEach(file =>
+                  fs.readdirSync(artifactsDir).forEach(file => {
+                    observer.next(`Copying ${file}`);
                     fs.copyFileSync(
                       resolve(artifactsDir, file),
                       resolve(basePath, "./docker/artifacts", file)
-                    )
-                  );
+                    );
+                  });
                   observer.complete();
-                  task.title = `${serviceName} updated`; // eslint-disable-line no-param-reassign
                 });
+              },
+            },
+            {
+              title: "Build image",
+              task: () => {
+                activateServices([serviceName]);
+                const buildObservable = execao("docker-compose", ["build", serviceName], {
+                  cwd: basePath,
+                });
+                buildObservable.subscribe({
+                  complete: () => {
+                    // eslint-disable-next-line no-param-reassign
+                    task.title = `Ready to deploy ${serviceName}`;
+                  },
+                });
+                return buildObservable;
               },
             },
           ]);
@@ -54,7 +71,9 @@ class Update extends Command {
       { concurrent: 2 }
     );
 
-    tasks.run().catch(console.error);
+    await tasks.run().catch(console.error);
+
+    await updateDeployment();
   }
 }
 
