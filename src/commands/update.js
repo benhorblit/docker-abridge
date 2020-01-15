bedd17fffd6377f1a2d0a72f9183916e7a59c250
Ben Horblit <benjamin@horblit.com>
2020-01-14 18:14:06 -0700

    Finalize use of listr

--- src/commands/update.js
+++ src/commands/update.js
@@ -1,10 +1,9 @@
 const { Command } = require("@oclif/command");
 const fs = require("fs");
-const path = require("path");
+const { resolve } = require("path");
 const execa = require("execa-output");
 const Listr = require("listr");
 const { Observable } = require("rxjs");
-// const Build = require("./build");
 const { getService, basePath } = require("../utils/compose-config");
 
 class Update extends Command {
@@ -15,33 +14,33 @@ class Update extends Command {
 
     const tasks = new Listr(
       requested.map(serviceName => ({
-        title: `Updating ${serviceName}`,
+        title: `Update ${serviceName}`,
         task: (context, task) => {
           const abridgeConfig = getService(serviceName)["docker-abridge"];
           return new Listr([
             {
-              title: "Executing build command",
+              title: "Execute build command",
               task: () => {
                 const [buildCommand, ...buildArgs] = abridgeConfig.build;
                 return execa(buildCommand, buildArgs, {
-                  cwd: path.resolve(basePath, abridgeConfig.context),
+                  cwd: resolve(basePath, abridgeConfig.context),
                 });
               },
             },
             {
-              title: "Copying artifacts",
+              title: "Copy artifacts",
               task: () => {
                 return new Observable(observer => {
                   observer.next("Copying...");
-                  const artifactsDir = path.resolve(
+                  const artifactsDir = resolve(
                     basePath,
                     abridgeConfig.context,
                     abridgeConfig.artifacts
                   );
                   fs.readdirSync(artifactsDir).forEach(file =>
                     fs.copyFileSync(
-                      path.resolve(artifactsDir, file),
-                      path.resolve(basePath, "./docker/artifacts", file)
+                      resolve(artifactsDir, file),
+                      resolve(basePath, "./docker/artifacts", file)
                     )
                   );
                   observer.complete();
@@ -56,28 +55,6 @@ class Update extends Command {
     );
 
     tasks.run().catch(console.error);
-
-    // await Promise.all(
-    //   requested.sort().map(async serviceName => {
-    //     const abridgeConfig = getService(serviceName)["docker-abridge"];
-
-    //     const [buildCommand, ...buildArgs] = abridgeConfig.build;
-    //     await execa(buildCommand, buildArgs, {
-    //       stdio: "inherit",
-    //       cwd: path.resolve(basePath, abridgeConfig.context),
-    //     });
-
-    //     const artifactsDir = path.resolve(basePath, abridgeConfig.context, abridgeConfig.artifacts);
-    //     fs.readdirSync(artifactsDir).forEach(file =>
-    //       fs.copyFileSync(
-    //         path.resolve(artifactsDir, file),
-    //         path.resolve(basePath, "./docker/artifacts", file)
-    //       )
-    //     );
-    //   })
-    // );
-
-    // Build.run(requested);
   }
 }
 
