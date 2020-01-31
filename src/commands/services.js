92d4d5ea002fa47cbee0c6ae459005b56fb454c3
Ben Horblit <benjamin@horblit.com>
2020-01-30 17:51:15 -0700

    Improve documentation for services command

--- src/commands/services.js
+++ src/commands/services.js
@@ -1,7 +1,8 @@
 const DockerComposeCommand = require("../utils/docker-compose-command");
 
 class Services extends DockerComposeCommand {
-  static description = "Lists the services currently present in the docker-compose.yml.";
+  static description = `Lists the services currently present in the docker-compose.yml.
+Equivalent to running 'docker-compose ps --services'.`;
 
   async run() {
     await this.dockerCompose("ps", "--services");
